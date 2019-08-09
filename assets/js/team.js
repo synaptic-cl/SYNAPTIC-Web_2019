347e7b46b6cae563c60f31d111ec7e31b37a7d5f
Nataly <nataly.medina@synaptic.cl>
2019-08-09 15:42:44 -0400

    modal and random card added

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,13 +1,13 @@
 function Shuffle(o) {
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
 
 function getUrlVars() {
-    var vars = {}, hash;
+    var vars = {},
+        hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for (var i = 0; i < hashes.length; i++) {
         hash = hashes[i].split('=');
         vars[hash[0]] = hash[1];
     }
@@ -15,17 +15,23 @@ function getUrlVars() {
 };
 
 function dashboard() {
-    $(document).ready(function () {
+    $(document).ready(function() {
         $.getJSON('team.json', function(data) {
-            var source   = document.getElementById("entry-template").innerHTML;
+            Handlebars.registerHelper('ifFind', function(arg1, arg2, options) {
+                return (arg1.indexOf(arg2) >= 0) ? options.fn(this) : options.inverse(this);
+            });
+            var source = document.getElementById("entry-template").innerHTML;
             var template = Handlebars.compile(source);
-            $('#team').html(template({ team: Shuffle(data) }));
+            var tu = data.pop();
+            var shuffledTeam = Shuffle(data);
+            shuffledTeam.push(tu)
+            $('#team').html(template({ team: shuffledTeam }));
         });
     });
 };
 
 function person() {
-    $(document).ready(function () {
+    $(document).ready(function() {
         var hashes = getUrlVars();
         var name = hashes.name;
         $.getJSON('team.json', function(data) {
@@ -36,7 +42,7 @@ function person() {
                 }
             })
             if (!person) document.location.href = '/dashboard.html'
-            var source   = document.getElementById("entry-template").innerHTML;
+            var source = document.getElementById("entry-template").innerHTML;
             Handlebars.registerHelper('ifFind', function(arg1, arg2, options) {
                 return (arg1.indexOf(arg2) >= 0) ? options.fn(this) : options.inverse(this);
             });
@@ -45,4 +51,3 @@ function person() {
         });
     });
 };
-
